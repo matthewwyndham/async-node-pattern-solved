45386871615a32e8029b7709601383082f44a35f
Vasiuk <iuriivasiuk@byui.edu>
2018-03-07 18:30:52 -0700

    changed to reading a file

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
-var http = require('http');
+var fs = require('fs');
 
-function getRecentNodeVersion(callback) {
+function readFileGetWord(callback) {
     console.log(0);
-    http.get('http://nodejs.org/dist/index.json', function (err, nodeVersions) {
+    fs.readFile('data.json', 'utf8',function (err, fileString) {
         console.log(0);
         if (err) {
             callback(err);
             // if this returned a value where would it go?
             return
         }
+        var data = JSON.parse(fileString);
         //this is weird for a callback to return a value, just think about it
-        var words = callback(null, nodeVersions[0].version);
-        console.log(words);
+        var words = callback(null, data[0]);
+        console.log("Value returned from callback inside readFileGetWord:", words);
         console.log(0);
 
         //where does this string go?
@@ -29,12 +30,12 @@ function getRecentNodeVersion(callback) {
 function addNumbers(a, b, callback) {
     console.log(0);
     var notANumber = callback(null, a + b);
-    console.log(notANumber);
+    console.log("Value returned from callback in addNumbers:", notANumber);
     console.log(0);
     //it is weird for the "node-pattern of handling async problems" to return something (don't do this) 
     //but I want you to think about this to fully understand async flow of execution
     //and to understand the difference between an async function and a callback
-    return 500;
+    return "dog";
 }
 
 
@@ -42,7 +43,7 @@ function start() {
     var text, number;
 
     console.log(0)
-    text = getRecentNodeVersion(function (err, nodeVersion) {
+    text = readFileGetWord(function (err, word) {
         console.log(0);
 
         if (err) {
@@ -51,7 +52,7 @@ function start() {
             return;
         }
 
-        console.log("Current Node Version:", nodeVersion);
+        console.log("Word from file:", word);
         console.log(0);
 
         //this return is also weird, just want you to think about it
@@ -66,15 +67,15 @@ function start() {
             return;
         }
         console.log(0);
-        console.log(sum);
+        console.log("Sum:", sum);
         //this return is also weird, just want you to think about it
         return "not a number";
     });
 
     console.log(0);
-    console.log(number);
+    console.log("Value returned from addNumbers:", number);
     console.log(0);
-    console.log(text);
+    console.log("Value returned from readFileGetWord:", text);
     console.log(0);
 }
 
